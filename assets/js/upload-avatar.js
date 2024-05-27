6bc59810cbde45ed4f7fd454b0a82de9969c5c25
konjkav <konjkav.git@gmail.com>
2024-05-27 05:24:54 +0330

    add
    add

---
+++ assets/js/upload-avatar.js
@@ -0,0 +1,15 @@
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#image-avatar').css('background-image', 'url('+e.target.result +')');
+            $('#image-avatar').hide();
+            $('#image-avatar').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+        console.log("ali")
+    }
+}
+$("#imageUpload").change(function() {
+    readURL(this);
+});
